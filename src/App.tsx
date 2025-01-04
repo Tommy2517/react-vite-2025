a5864b755413e147eeff0c8f564d8e8c4e26497c
Tommy2517 <artem07stu@gmail.com>
2025-01-04 20:33:34 +0100

    clear project for practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css'
-import {products} from "./data/productsList.ts";
-import {IProduct} from "./modules/product.ts";
-import MyProduct from "./components/my-product/MyProduct.tsx";
 //branch lesson 1
 function App() {
 
   return (
     <>
-      {products.map((product:IProduct, index)=><MyProduct key={index} product={product}/>)}
     </>
   )
 }
