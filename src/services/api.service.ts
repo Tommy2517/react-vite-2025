87e0877205404e884fb77a002a21d5233cc2b989
Tommy2517 <artem07stu@gmail.com>
2025-01-05 23:57:04 +0100

    lesson-2-4-env-

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,6 @@
 import {IUser} from "../models/users/IUser.ts";
 
 export const getUsers = async ():Promise<IUser[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users')
+    return await fetch(import.meta.env.VITE_API_URL)
         .then(value => value.json())
 }
