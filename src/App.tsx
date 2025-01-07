cfe7f937b9f55011fd73aafadfaf7e7a8dd76c7b
Tommy2517 <artem07stu@gmail.com>
2025-01-07 23:28:46 +0100

    resolved-homework2-fix-toggle-App

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ import {useState} from "react";
 function App() {
     const [showTodoComponent, setShowTodoComponent] = useState(false)
     const [buttonName, setButtonName] = useState<string>('')
-    const toggle = (buttonName:string) => {
-        setShowTodoComponent(!showTodoComponent)
-        setButtonName(buttonName)
+    const toggle = (ChooseButtonName:string) => {
+        //1 кнопка нажата из плейсхолдера в плейсхолдер
+        //2 кнопка отжата из деми в деми
+        //3 кнопка нажата из деми в плейсхолдер
+        //4 кнопка нажата из плейсхолдера в деми
+        //5 первое нажатие
+        if (!buttonName) {
+            setShowTodoComponent(!showTodoComponent)
+            setButtonName(ChooseButtonName)
+        }
+        if (buttonName === ChooseButtonName){
+            setShowTodoComponent(!showTodoComponent)
+            setButtonName('')
+        }
+        else {
+            setButtonName(ChooseButtonName)
+        }
     }
     return (
         <>
