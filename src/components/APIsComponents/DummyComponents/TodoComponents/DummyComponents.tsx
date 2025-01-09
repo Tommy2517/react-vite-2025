ad90be76c4e663dabd6600d4cac336b9e186caea
Tommy2517 <artem07stu@gmail.com>
2025-01-09 18:13:16 +0100

    resolved-homework2-add-loading

--- src/components/APIsComponents/DummyComponents/TodoComponents/DummyComponents.tsx
+++ src/components/APIsComponents/DummyComponents/TodoComponents/DummyComponents.tsx
@@ -10,10 +10,11 @@ const DummyComponents = () => {
         getTodoDataDummy()
             .then(response => setTodos(response))
     },[])
+    if(!todos) return <div>loaging...</div>
 
     return (
         <div className={'flex flex-wrap gap-4 p-6 justify-center text-lg font-serif'}>
-            {todos?.todos.map(item => <DummyComponent key={item.id} todo={item}/>)}
+            {todos.todos.map(item => <DummyComponent key={item.id} todo={item}/>)}
         </div>
     );
 };
--- src/components/APIsComponents/PlaceholderComponents/TodoComponents/PlaceholderComponents.tsx
+++ src/components/APIsComponents/PlaceholderComponents/TodoComponents/PlaceholderComponents.tsx
@@ -4,12 +4,13 @@ import {IPlaceholderTodo} from "../../../../models/IPlaceholderTodo.ts";
 import PlaceholderComponent from "../TodoComponent/PlaceholderComponent.tsx";
 
 const PlaceholderComponents = () => {
-    const [todos, setTodos] = useState<IPlaceholderTodo[]>([])
+    const [todos, setTodos] = useState<IPlaceholderTodo[] | null>(null)
 
     useEffect(()=>{
         getTodoDataPlaceholder()
             .then(response => setTodos(response))
     },[])
+    if(!todos) return <div>loaging...</div>
 
     return (
         <div className={'flex flex-wrap gap-4 p-6 justify-center text-lg font-serif'}>
