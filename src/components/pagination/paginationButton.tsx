8d30a5854f4536906a1385ca1faefacedc72ddb2
Tommy2517 <artem07stu@gmail.com>
2025-01-23 11:35:41 +0100

    lesson-4-2-pagination-without-btnDisabled

--- src/components/pagination/paginationButton.tsx
+++ src/components/pagination/paginationButton.tsx
@@ -4,30 +4,30 @@ type prop = {
     total_pages:number
 }
 const PaginationButtons:FC<prop> = ({total_pages}) => {
-    const [isDisabledPrev, setIsDisabledPrev] = useState<boolean>(true)
+    const [isDisabledPrev, setIsDisabledPrev] = useState<boolean>(false)
     const [isDisabledNext, setIsDisabledNext] = useState<boolean>(false)
     const [query, setQuery] = useSearchParams({page: '1'});
 
     return (
         <div>
-            <button disabled={isDisabledPrev} onClick={() => {
+            <button className={'border-2 border-amber-500 m-5 p-2'} disabled={isDisabledPrev} onClick={() => {
                 const page = query.get('page');
-                if (page) {
+                if (page && +page > 1) {
                     let currentPage = +page
                     setQuery({page: (--currentPage).toString()})
-                    if (currentPage == 1) setIsDisabledPrev(true)
-                    if (currentPage < total_pages) setIsDisabledNext(false)
+                    // if (currentPage == 1) setIsDisabledPrev(true)
+                    // if (currentPage < total_pages) setIsDisabledNext(false)
                 }
             }}>prew
             </button>
 
-            <button disabled={isDisabledNext} onClick={() => {
+            <button className={'border-2 border-amber-500 m-5 p-2'} disabled={isDisabledNext} onClick={() => {
                 const page = query.get('page');
-                if (page) {
+                if (page && +page < total_pages) {
                     let currentPage = +page
                     setQuery({page: (++currentPage).toString()})
-                    if (currentPage > 1) setIsDisabledPrev(false)
-                    if (currentPage == total_pages) setIsDisabledNext(true)
+                    // if (currentPage > 1) setIsDisabledPrev(false)
+                    // if (currentPage == total_pages) setIsDisabledNext(true)
                 }
             }}>next
             </button>
