1ed1f86c82ca54702d8a495292601c0731c0dea9
Tommy2517 <artem07stu@gmail.com>
2025-01-26 16:48:30 +0100

    lesson-5-4-validate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import ControlFormComponent from "./components/FormComponents/ControlForm1/ControlFormComponent.tsx";
 import RegisterFormComponent from "./components/FormComponents/RegisterFormComponent/RegisterFormComponent.tsx";
+import ValidateFormComponent from "./components/FormComponents/ValidateFormomponent/ValidateFormComponent.tsx";
 
 function App() {
 
   return (
     <>
         {/*<ControlFormComponent/>*/}
-        <RegisterFormComponent/>
+        {/*<RegisterFormComponent/>*/}
+        <ValidateFormComponent/>
     </>
   )
 }
